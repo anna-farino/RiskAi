16766583dd302fa8ed1573dbf9bc3fb2814c5d97
Roberto Loss <robertoloss@gmail.com>
2025-05-03 17:38:53 -0400

    dotenvConfig with plain __dirname:

--- backend/utils/dotenv-config.ts
+++ backend/utils/dotenv-config.ts
@@ -1,10 +1,6 @@
 import path from 'path'
-import { fileURLToPath } from 'url';
 
 export default function dotenvConfig(dotenv: any) {
-
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
 
   dotenv.config({
     path: path.resolve(__dirname, '../.env.local'), // adjust if deeper
