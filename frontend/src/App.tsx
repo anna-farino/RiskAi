c82af1808ad8226babae8ccce3633c13e44e44fd
robertoloss-altairtek <rloss@altairtek.com>
2025-05-10 15:58:38 +0000

    test (pushing to dev)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from './components/theme-provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from '@/components/ui/toaster'
 import { router } from './router/router'
-
+//comment test
 const queryClient = new QueryClient()
 
 export default function App() {
