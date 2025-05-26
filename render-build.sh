24ea1dee1f5c9a46a65cc460076fa3031d712aa9
Roberto Loss <robertoloss@gmail.com>
2025-05-26 15:23:26 -0400

    restore render_build

--- render-build.sh
+++ render-build.sh
@@ -3,26 +3,10 @@ set -o errexit
 
 npm install
 
-# Set cache directories
-export PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-export XDG_CACHE_HOME=/opt/render/.cache
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer npx puppeteer browsers install chrome
+mkdir -p .cache/puppeteer
+cp -R /opt/render/.cache/puppeteer/chrome .cache/puppeteer/
 
-# Only copy if source and destination are different
-if [[ ! -d "$PUPPETEER_CACHE_DIR/chrome" ]]; then 
-  echo "...Copying Puppeteer Cache from Build Cache"
-  cp -R "$XDG_CACHE_HOME/puppeteer/chrome" "$PUPPETEER_CACHE_DIR/"
-elif [[ "$PUPPETEER_CACHE_DIR" != "$XDG_CACHE_HOME/puppeteer" ]]; then
-  echo "...Storing Puppeteer Cache in Build Cache"
-  cp -R "$PUPPETEER_CACHE_DIR/chrome" "$XDG_CACHE_HOME/puppeteer/"
-else
-  echo "...Skipping copy: source and destination are the same"
-fi
-
-# PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer npx puppeteer browsers install chrome
-# mkdir -p .cache/puppeteer
-# cp -R /opt/render/.cache/puppeteer/chrome .cache/puppeteer/
-#
-npx puppeteer browsers install chrome
 npx drizzle-kit migrate --config=drizzle.config.ts
 npm run build -w backend
 
