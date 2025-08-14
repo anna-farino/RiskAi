6b0c77af44258df547ff0e34ad2a1d97980112ca
Roberto Loss <robertoloss@gmail.com>
2025-08-14 13:14:24 -0400

    fix (drizzle kit)

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,10 @@
-import { defineConfig } from 'drizzle-kit';
-import dotenv from 'dotenv';
-
-dotenv.config()
-
 const isAzure = process.env.IS_AZURE;
 
-export default defineConfig({
+export default {
   dialect: 'postgresql',
   schema: isAzure ? './shared/db/schema/*' : './shared/db/schema/*',
   out: isAzure ? './backend/db/migrations' : './backend/db/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
