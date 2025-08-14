6c68d8a364b2d5c059f74854d94718fee967f534
Roberto Loss <robertoloss@gmail.com>
2025-08-14 12:46:43 -0400

    fix migration config paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,8 +7,8 @@ const isAzure = process.env.IS_AZURE;
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: isAzure ? '../shared/db/schema/*' : './shared/db/schema/*',
-  out: isAzure ? './db/migrations' :'./backend/db/migrations',
+  schema: isAzure ? './shared/db/schema/*' : './shared/db/schema/*',
+  out: isAzure ? './backend/db/migrations' :'./backend/db/migrations',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
