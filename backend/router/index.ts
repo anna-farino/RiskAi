0f123de722bcbdf35f8adabfa467d1cc42260dd5
Roberto Loss <robertoloss@gmail.com>
2025-08-19 10:22:58 -0400

    Pushing new global tables

--- backend/router/index.ts
+++ backend/router/index.ts
@@ -6,7 +6,7 @@ import { handleGetRoles } from '../handlers/roles';
 import { authRouter } from './routes/auth';
 import { usersRouter } from './routes/users';
 import { noSimpleRequests } from '../middleware/no-simple-requests';
-import { doubleCsrfProtection } from '../middleware/csrf';
+import { doubleCsrfProtection, generateToken } from '../middleware/csrf';
 import { newsRouter } from '../apps/news-radar/router';
 import { rateLimit } from 'express-rate-limit'
 import { rateLimitConfig } from 'backend/utils/rate-limit-config';
@@ -18,16 +18,9 @@ import { auth0CheckJwt, jwtErrorHandler } from 'backend/middleware/auth0';
 import { testDatadomeBypass } from 'backend/handlers/test-datadome';
 import { auth0middleware } from 'backend/middleware/auth0middleware';
 import { handleChangePassword } from 'backend/handlers/auth0/change-password';
-import { NextFunction } from 'http-proxy-middleware/dist/types';
 
 const limiter = rateLimit(rateLimitConfig)
 const router = Router();
-
-router.use((req: Request, _: Response, next: NextFunction)=>{
-  //console.log("Server hit")
-  //console.log("req.headers.authorization", req.headers.authorization)
-  next()
-})
 
 // TEST route
 router.get('/test', limiter, handleTest)
@@ -41,15 +34,14 @@ router.get('/test-datadome-bypass', testDatadomeBypass)
 // AUTH
 router.use('/auth', limiter, authRouter)
 
-
 // ================================================
 // PROTECTIONS ====================================
 // ================================================
+
 router.use(auth0CheckJwt)
 router.use(jwtErrorHandler)
-//router.use(doubleCsrfProtection)
 router.use(noSimpleRequests)
-//router.use(verifyToken)
+
 router.use(auth0middleware)
 
 
