5724c1a4b8dd5af298f647ea7e1511a55190fe1c
robertoloss-altairtek <rloss@altairtek.com>
2025-08-21 15:25:03 -0400

    Drop not null for scraping-errors table
    * auth0 first steps
    
    * success with test route
    
    * clean basic flow (login, fetch, logout)
    
    * fix
    
    * cleanup (html files)
    
    * update
    
    * login/logout working with all routes protected
    
    * fix redirect component
    
    * update auth0 redirections
    
    * update
    
    * update
    
    * password reset (to add: redirection after psw change)
    
    * files from camel case to kebab case
    
    * new migration 25 for scraping-error-logs table
    
    * add auth0 config
    
    * fix
    
    * use auth0_id to identify users:
    
    * deleted auth0config folder and added env variables to gh actions for frontend (conditional environment to be added later)
    
    * update Login component with env var for audience
    
    * fix hard-coded audience problem
    
    * fix hard-coded callback_url at logout
    
    * add root route and redirect
    
    * hide logout button at login route
    
    * update github action for fronted with env variables depending on github environment
    
    * fix yaml file for frontend gh action
    
    * fix yaml file for frontend gh action
    
    * fix yaml file
    
    * update vite server url dev in yaml
    
    * added withUserContext for keywords and reports
    
    * Add RLS to keywords, threat_keywords, and reports: crud operations can be performed only if user_id = app.current_user_id
    
    * Update RLS
    
    * Encryption/Decryption is backward compatible with non-encrypted data && OLD KEY vs NEW KEY
    
    * changed yaml for github action
    
    * fix encryption.ts
    
    * backend cleanup
    
    * fixing mfa
    
    * fix mfa switch in settings (backend)
    
    * new global tables for new scraping system
    
    * Pushing new global tables
    
    * add check health route
    
    * fix logic for db health check
    
    * fix logic for db health check
    
    * clean up server logs
    
    * clean up frontend logs
    
    * drop not null for user_id in scraping-error-logs
    
    ---------
    
    Co-authored-by: Roberto Loss <robertoloss@gmail.com>

--- shared/db/schema/scraping-error-logs.ts
+++ shared/db/schema/scraping-error-logs.ts
@@ -29,7 +29,7 @@ export const errorTypeEnum = pgEnum('error_type', [
 
 export const scrapingErrorLogs = pgTable('scraping_error_logs', {
   id: uuid('id').primaryKey().defaultRandom(),
-  userId: uuid('user_id').notNull().references(() => users.id),
+  userId: uuid('user_id').references(() => users.id),
   sourceId: text('source_id'),
   sourceUrl: text('source_url').notNull(),
   appType: appTypeEnum('app_type').notNull(),
